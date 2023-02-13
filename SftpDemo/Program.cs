5d608db96b5146aef72440d87aeba1e3bc229be6
NurBr <NurBr@BROCK>
2023-02-13 09:45:15 +0200

    Add project files.

---
+++ SftpDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SftpDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //upload sftp 
+            using (SftpClient client = new SftpClient(new PasswordConnectionInfo("192.168.1.102", "tester", "password")))
+            { client.Connect(); }
+        }
+    }
+}
